ec63a613b98befec5df91d6e5d5ca852b84c1bcf
Woonjang Ahn <hodol@woowahan.com>
2021-01-10 05:06:10 +0900

    feat: add cors settings

---
+++ backend/src/main/java/koreahacks/woowabros/uniconn/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package koreahacks.woowabros.uniconn;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("https://uniconn.me")
+                .maxAge(3600);
+    }
+}
