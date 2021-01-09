e68c7c7ef09cff948cd14a778ac641f73811b3b2
dd <dqrd123@gmail.com>
2021-01-09 10:54:13 +0900

    feat: basic architecture

---
+++ backend/src/main/java/koreahacks/woowabros/uniconn/member/presentation/MemberController.java
@@ -0,0 +1,27 @@
+package koreahacks.woowabros.uniconn.member.presentation;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import koreahacks.woowabros.uniconn.member.application.MemberService;
+import koreahacks.woowabros.uniconn.member.domain.Member;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/")
+    public Mono<MemberResponse> create(@RequestBody MemberCreateRequest request) {
+        return memberService.create(request);
+    }
+
+
+
+}
