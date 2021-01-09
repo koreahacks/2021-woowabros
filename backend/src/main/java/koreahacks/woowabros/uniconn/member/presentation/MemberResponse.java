432e1ad653fe76348675e4eb79612549ebb71158
dd <dqrd123@gmail.com>
2021-01-10 07:19:39 +0900

    fix: verify가 정상적으로 동작하도록 수정

--- backend/src/main/java/koreahacks/woowabros/uniconn/member/presentation/MemberResponse.java
+++
@@ -1,9 +0,0 @@
-package koreahacks.woowabros.uniconn.member.presentation;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class MemberResponse {
-}
