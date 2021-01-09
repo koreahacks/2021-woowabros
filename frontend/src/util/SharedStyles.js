00e7588c79961a33aa209672880338cd8907b6c6
hurjaehyeok <Jack2ee.dev@gmail.com>
2021-01-09 14:53:22 +0900

    feat: frame main page

---
+++ frontend/src/util/SharedStyles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const AppWrapper = styled.div`
+  padding: 1rem 0.5rem;
+`;
+
+export const SharedPurpleWrapper = styled.div`
+  height: 100%;
+  padding: 1rem;
+  display: flex;
+  align-items: stretch;
+  justify-content: center;
+  background-color: #d8bfd8;
+  border: 1px solid #aa8fbf;
+  border-radius: 0.5rem;
+  margin-top: 1.5rem;
+`;
