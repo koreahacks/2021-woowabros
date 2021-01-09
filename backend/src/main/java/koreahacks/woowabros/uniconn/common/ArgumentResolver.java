6a4699ba6a6f9e57c6e31183245c6d438e2dacb2
BEOMJUN KIM <43440195+ddaaac@users.noreply.github.com>
2021-01-10 01:14:32 +0900

    temp: 임시 테스트 메일 발급 (#22)

--- backend/src/main/java/koreahacks/woowabros/uniconn/common/ArgumentResolver.java
+++ backend/src/main/java/koreahacks/woowabros/uniconn/common/ArgumentResolver.java
@@ -1,13 +1,13 @@
 package koreahacks.woowabros.uniconn.common;
 
+import koreahacks.woowabros.uniconn.member.application.MemberService;
+import koreahacks.woowabros.uniconn.member.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.BindingContext;
 import org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver;
 import org.springframework.web.server.ServerWebExchange;
-
-import koreahacks.woowabros.uniconn.member.application.MemberService;
-import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
@@ -22,11 +22,16 @@ public class ArgumentResolver implements HandlerMethodArgumentResolver {
     }
 
     @Override
-    public Mono<Object> resolveArgument(MethodParameter parameter, BindingContext bindingContext,
-        ServerWebExchange exchange) {
+    public Mono<Object> resolveArgument(
+            MethodParameter parameter,
+            BindingContext bindingContext,
+            ServerWebExchange exchange
+    ) {
         try {
             String email = exchange.getAttributeOrDefault("email", "");
-
+            if ("test-email.com".equals(email)) {
+                return Mono.just(Member.builder().id("test-id").isVerified(true).nickname("test-nickname").email("test-email.com").build());
+            }
             return memberService.findFirstByEmail(email).map(member -> member);
         } catch (Exception e) {
             throw new IllegalArgumentException();
