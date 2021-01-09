a02b4af4d311d9cc2e688194ae136169b289bbcd
dd <dqrd123@gmail.com>
2021-01-10 07:47:49 +0900

    feat: verify duplicated email member

--- backend/src/main/java/koreahacks/woowabros/uniconn/member/domain/MemberRepository.java
+++ backend/src/main/java/koreahacks/woowabros/uniconn/member/domain/MemberRepository.java
@@ -2,10 +2,13 @@ package koreahacks.woowabros.uniconn.member.domain;
 
 import org.springframework.data.elasticsearch.repository.ReactiveElasticsearchRepository;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface MemberRepository extends ReactiveElasticsearchRepository<Member, String> {
     Mono<Member> findFirstByAuthCode(String authCode);
 
     Mono<Member> findFirstByEmail(String email);
+
+    Flux<Member> findByEmail(String email);
 }
