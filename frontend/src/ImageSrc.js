2ab32fb2a9cb88009ff0c62e2c1981e837e23139
hurjaehyeok <Jack2ee.dev@gmail.com>
2021-01-09 14:53:22 +0900

    feat: frame main page

---
+++ frontend/src/ImageSrc.js
@@ -0,0 +1,17 @@
+const { PUBLIC_URL } = process.env;
+
+const ImageSrc = {
+  ANONYMOUS: PUBLIC_URL + "anonymous.svg",
+  BACK: PUBLIC_URL + "back.svg",
+  CHECKBOX_FILL: PUBLIC_URL + "checkbox_fill.svg",
+  CHECKBOX: PUBLIC_URL + "checkbox.svg",
+  IMAGE: PUBLIC_URL + "image.svg",
+  NAMED: PUBLIC_URL + "named.svg",
+  THUMBS_DOWN_FILL: PUBLIC_URL + "thumbs_down_fill.svg",
+  THUMBS_DOWN: PUBLIC_URL + "thumbs_down.svg",
+  THUMBS_UP_FILL: PUBLIC_URL + "thumbs_up_fill.svg",
+  THUMBS_UP: PUBLIC_URL + "thumbs_up.svg",
+  UNICORN: PUBLIC_URL + "unicorn.svg",
+};
+
+export default ImageSrc;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Header from "../components/home/Header";
+import UserSummary from "../components/home/UserSummary";
+import { AppWrapper } from "../util/SharedStyles";
+import Post from "../components/home/PostButtons";
+import PostSummary from "../components/home/PostSummary";
 
 const Home = () => {
-  return <>홈</>;
+  const [userSummary, setUserSummary] = useState({
+    school: "한국대학교",
+    major: "컴퓨터학과",
+    name: "김경",
+    id: 1,
+    rank: 0.00001,
+  });
+
+  const [namedPostSummary, setNamedPostSummary] = useState([
+    {
+      id: 1,
+      summary: "아니 로렘입섬은 어떻게 쓰는거야!",
+    },
+    {
+      id: 1,
+      summary: "아니 로렘입섬은 어떻게 쓰는거야!",
+    },
+    {
+      id: 1,
+      summary: "아니 로렘입섬은 어떻게 쓰는거야!",
+    },
+  ]);
+
+  const [anonymousPostSummary, setAnonymousPostSummary] = useState([
+    {
+      id: 1,
+      summary: "아니 로렘입섬은 어떻게 쓰는거야!",
+    },
+    {
+      id: 1,
+      summary: "아니 로렘입섬은 어떻게 쓰는거야!",
+    },
+    {
+      id: 1,
+      summary: "아니 로렘입섬은 어떻게 쓰는거야!",
+    },
+  ]);
+
+  return (
+    <AppWrapper>
+      <Header />
+      <UserSummary data={{ userSummary }} />
+      <Post />
+      <PostSummary data={{ title: "실명 게시판", summary: namedPostSummary }} />
+      <PostSummary
+        data={{ title: "익명 게시판", summary: anonymousPostSummary }}
+      />
+    </AppWrapper>
+  );
 };
 
 export default Home;
